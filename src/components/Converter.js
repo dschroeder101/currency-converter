525a507759e4696eb1edddf4112657af2926f1d7
Daniel Schroeder <dschroeder101@gmail.com>
2020-06-28 12:03:56 -0700

    Initial commit

---
+++ src/components/Converter.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Jumbotron } from "react-bootstrap";
+
+class Converter extends Component {
+  render() {
+    return <Jumbotron></Jumbotron>;
+  }
+}
+
+export default Converter;
