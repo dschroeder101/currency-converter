573adaf65c0e8172f8381c95f36c7775d6391a95
Daniel Schroeder <dschroeder101@gmail.com>
2020-06-29 11:05:46 -0700

    Added Redis caching mechanism

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import ReactDOMServer from "react-dom/server";
 import bodyParser from "body-parser";
 import axios from "axios";
 import Big from "big.js";
+import redis from "redis";
+import crypto from "crypto";
 
 import App from "../src/App";
 
@@ -13,6 +15,15 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const redisClient = redis.createClient(process.env.REDIS_URL);
+
+redisClient.on("connect", function () {
+  console.log("Redis client connected");
+});
+
+redisClient.on("error", function (err) {
+  console.log("Something went wrong" + err);
+});
 
 app.use(express.static("./build"));
 app.use(bodyParser.json());
@@ -44,40 +55,59 @@ function calculateResult(baseAmount, baseToEuroRate, euroToTargetRate) {
 }
 
 app.post("/convert", (req, res) => {
-  console.log(req.body);
   let formattedDate =
     req.body.date.year + "-" + req.body.date.month + "-" + req.body.date.day;
 
-  axios
-    .get(
-      `http://data.fixer.io/api/${formattedDate}?access_key=${process.env.FIXER_KEY}&symbols=${req.body.baseCurrency},${req.body.targetCurrency}`
-    )
-    .then(
-      (fixerResponse) => {
-        console.log(fixerResponse);
-        if (fixerResponse.data.success) {
-          const result = calculateResult(
-            req.body.amount,
-            fixerResponse.data.rates[req.body.baseCurrency],
-            fixerResponse.data.rates[req.body.targetCurrency]
-          );
-          return res.status(200).send(result);
-        } else {
-          return res
-            .status(500)
-            .send(
-              "Error making fixer request - " +
-                fixerResponse.data.error.type +
-                " - " +
-                fixerResponse.data.error.info
-            );
-        }
-      },
-      (error) => {
-        console.log(error);
-        res.status(500).send("500 - Server error occurred");
-      }
-    );
+  let hash = crypto
+    .createHash("md5")
+    .update(formattedDate + req.body.baseCurrency + req.body.targetCurrency)
+    .digest("hex");
+
+  redisClient.get(hash, function (error, redisResult) {
+    if (redisResult) {
+      console.log("Returning cached rates...");
+      redisResult = JSON.parse(redisResult);
+      const result = calculateResult(
+        req.body.amount,
+        redisResult.rates[req.body.baseCurrency],
+        redisResult.rates[req.body.targetCurrency]
+      );
+      return res.status(200).send(result);
+    } else {
+      console.log("Querying API for rates...");
+      axios
+        .get(
+          `http://data.fixer.io/api/${formattedDate}?access_key=${process.env.FIXER_KEY}&symbols=${req.body.baseCurrency},${req.body.targetCurrency}`
+        )
+        .then(
+          (fixerResponse) => {
+            if (fixerResponse.data.success) {
+              redisClient.set(hash, JSON.stringify(fixerResponse.data));
+
+              const result = calculateResult(
+                req.body.amount,
+                fixerResponse.data.rates[req.body.baseCurrency],
+                fixerResponse.data.rates[req.body.targetCurrency]
+              );
+              return res.status(200).send(result);
+            } else {
+              return res
+                .status(500)
+                .send(
+                  "Error making fixer request - " +
+                    fixerResponse.data.error.type +
+                    " - " +
+                    fixerResponse.data.error.info
+                );
+            }
+          },
+          (error) => {
+            console.log(error);
+            res.status(500).send("500 - Server error occurred");
+          }
+        );
+    }
+  });
 });
 
 app.listen(PORT, () => {
